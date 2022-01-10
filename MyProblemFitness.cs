6afed846f88fe67302c9d56fe276e6870cb20076
Alan Lo <alanlo@gammonconstruction.com>
2022-01-10 10:17:00 +0800

    Add project files.

---
+++ MyProblemFitness.cs
@@ -0,0 +1,14 @@
+﻿using GeneticSharp.Domain.Chromosomes;
+using GeneticSharp.Domain.Fitnesses;
+using System;
+
+namespace GAConsoleApp
+{
+    class MyProblemFitness : IFitness
+    {
+        public double Evaluate(IChromosome chromosome)
+        {
+            throw new NotImplementedException("// TODO: Evaluate the fitness of the chromosome.");
+        }
+    }
+}
